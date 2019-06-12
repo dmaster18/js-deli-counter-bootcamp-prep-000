fe459b59887898574d139333451bf2980fe2d44e
David Strauch <dmaster18@gmail.com>
2019-06-12 18:23:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine , personsName) {
-  for (var i = 0; )
+  for (var i = 0; i <)
   return `Welcome, ${personsName}. You are number ${katzDeliLine} in line.`
 }
 
