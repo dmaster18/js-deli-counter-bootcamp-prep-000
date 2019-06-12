8c899514f3b3eed95e31c483557a83ed813784e0
David Strauch <dmaster18@gmail.com>
2019-06-12 18:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 
 function takeANumber(currentLine, personsName) {
-  return `Welcome, ${personsName}
+  return `Welcome, ${personsName}. You are number ${currentLine} in line.`
   
 }
