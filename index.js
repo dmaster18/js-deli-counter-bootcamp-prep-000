26d697f37450fb41c1f0a776b483eaf0b9d21938
David Strauch <dmaster18@gmail.com>
2019-06-12 18:56:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine) {
-  if(katzDeliLine.length===)
+  if(katzDeliLine.length===0)
   return `The line is currently: ${
     
   }
