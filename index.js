3f9052c4d13072c33fb261ebb5557f7feb3604f4
David Strauch <dmaster18@gmail.com>
2019-06-12 18:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function takeANumber(katzDeliLine , personsName) {
   for (var i = 0; i < katzDeliLine.length; i++){
      return `Welcome, ${personsName}. You are number ${i+1} in line.`
   }
- 
 }
 
 function nowServing(katzDeliLine){
