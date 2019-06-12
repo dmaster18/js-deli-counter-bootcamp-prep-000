4d53b64363bf29a685530332afc75cde1545fe42
David Strauch <dmaster18@gmail.com>
2019-06-12 18:59:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty";
   }
   else{
-  for (var i=0; i < )  
+  for (var i=0; i < katzDeliLine.length; i++ )  
   var announcement = `The line is currently: ${
   }
 }
